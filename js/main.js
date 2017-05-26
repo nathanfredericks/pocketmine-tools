5d8576367910425ebfb920b22f3ac77d80d9cba5
Ad5001 <mail@ad5001.eu>
2017-05-26 12:10:23 +0200

    Mobile UI.

--- js/main.js
+++ js/main.js
@@ -8,5 +8,13 @@ $(document).ready(
                     // $('input:submit').removeAttr('disabled'); 
                 } 
             }
-            );
+        );
+        // Preparing mobile UI.
+        $("iframe#content").load(
+            function(){
+                if(window.innerWidth < 800) { // Mobile, needs centering
+                    but = document.getElementById("UploadButton").style.margin = "0 auto";
+                }
+            }
+        )
     });
