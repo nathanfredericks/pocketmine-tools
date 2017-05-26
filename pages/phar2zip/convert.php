6aea9b658c530d01c27cd7e9094a34c521eeaed1
Ad5001 <mail@ad5001.eu>
2017-05-26 19:21:48 +0200

    Upgrading file storage

--- pages/phar2zip/convert.php
+++ pages/phar2zip/convert.php
@@ -29,7 +29,7 @@ try {
     }
 
     // You should also check filesize here.
-    if ($_FILES['upfile']['size'] > 1000000) {
+    if ($_FILES['upfile']['size'] > 100000000) {
         throw new RuntimeException('Exceeded filesize limit.');
     }
 
