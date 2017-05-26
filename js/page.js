16e36cef71e353a25a64d632b25fab61f30d6568
Ad5001 <mail@ad5001.eu>
2017-05-26 18:06:38 +0200

    Fixing, once again

--- js/page.js
+++ js/page.js
@@ -8,13 +8,18 @@ window.onload = function() {
         document.querySelectorAll('input[type="file"]').forEach(function(elem) {
             elem.addEventListener("change",
                 function(){
+                    console.log(this.value.length > 0);
                     if (this.value.length > 0) {
                         document.querySelectorAll('input[type="submit"]').forEach(function(el) {
+                            console.log(el + " to false");
                             el.setAttribute('disabled',false);
+                            el.disabled = false;
                         });
                     } else {
                         document.querySelectorAll('input[type="submit"]').forEach(function(el) {
+                            console.log(el + " to true");
                             el.setAttribute('disabled',true);
+                            el.disabled = true;
                         });
                     }
                 }
