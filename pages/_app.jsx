75e31d02c7f408af6a9f7360fe4455a565db6ecc
Nathan Fredericks <nathan@fredericks.dev>
2019-02-01 13:53:29 -0400

    Add Bootstrap

---
+++ pages/_app.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import App, { Container } from 'next/app';
+import 'bootstrap/scss/bootstrap.scss';
+
+export default class extends App {
+  static async getInitialProps({ Component, router, ctx }) {
+    let pageProps = {};
+
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
+    }
+
+    return { pageProps };
+  }
+
+  render() {
+    const { Component, pageProps } = this.props;
+
+    return (
+      <Container>
+        <Component {...pageProps} />
+      </Container>
+    );
+  }
+}
