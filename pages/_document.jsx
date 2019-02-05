05ead365c820b0965bc86ed75044b408792ab0ed
Nathan Fredericks <nathan@fredericks.dev>
2019-02-05 17:37:59 -0400

    Revert "Revert "Add Google Analytics""
    This reverts commit 58b1c96564a22b820089c052224bdb1a931cea43.

---
+++ pages/_document.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import Document, { Head, Main, NextScript } from 'next/document';
+
+import { GA_TRACKING_ID } from '../utils/gtag';
+
+export default class extends Document {
+  static getInitialProps({ renderPage }) {
+    const { html, head, errorHtml, chunks } = renderPage();
+
+    return { html, head, errorHtml, chunks };
+  }
+
+  render = () => (
+    <html lang="en">
+      <Head>
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${GA_TRACKING_ID}');
+            `,
+          }}
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </html>
+  );
+}
