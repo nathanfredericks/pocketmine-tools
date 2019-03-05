7c366bb3307100d49bc198017bad8d9d4d36a107
Nathan Fredericks <nathan@fredericks.dev>
2019-03-05 19:44:22 -0400

    Revert "Security: changing phar building paths."
    This reverts commit 8145fe9a04aae2f2338d1de6c5f9a7dc165abb72.

--- pages/zip2phar/download.php
+++ pages/zip2phar/download.php
@@ -1,7 +1,7 @@
 <?php
 
 global $ROOT_DIR;
-$ROOT_DIR = "/tmp";
+$ROOT_DIR = implode(DIRECTORY_SEPARATOR, array_slice(explode(DIRECTORY_SEPARATOR, __DIR__), 0, count(explode(DIRECTORY_SEPARATOR, __DIR__)) - 2));
 
 if(!isset($_GET["n"]) || !file_exists($ROOT_DIR . "/data/phars/" . $_GET["n"] . ".phar")) {
     header("Location: index.html");
