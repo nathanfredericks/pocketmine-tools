7455a5f9688aa6d5b5f3710cf9b8eb79caf998a8
Nathan Fredericks <nathan@fredericks.dev>
2019-12-14 19:21:54 -0400

    Remove Fathom

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -14,23 +14,6 @@ export default class extends Document {
         <script
           dangerouslySetInnerHTML={{
             __html: `
-(function(f, a, t, h, o, m){
-  a[h]=a[h]||function(){
-    (a[h].q=a[h].q||[]).push(arguments)
-  };
-  o=f.createElement('script'),
-  m=f.getElementsByTagName('script')[0];
-  o.async=1; o.src=t; o.id='fathom-script';
-  m.parentNode.insertBefore(o,m)
-})(document, window, '//fathom.nathfreder.dev/tracker.js', 'fathom');
-fathom('set', 'siteId', 'OCHDV');
-fathom('trackPageview');
-            `,
-          }}
-        />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
             var _rollbarConfig = {
               accessToken: "f8e4b76719724ce1bd3e06a79c62b41a",
               captureUncaught: true,
