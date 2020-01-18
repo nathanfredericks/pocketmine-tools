a8e7b5c656bc3f29697a275407ee64cc43bea7ae
Nathan Fredericks <nathan@fredericks.dev>
2020-01-18 15:08:01 -0400

    Rocket.chat

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -23,12 +23,15 @@ export default class extends Document {
         <script
           dangerouslySetInnerHTML={{
             __html: `
-            window.intergramId = "678904684"
+            (function(w, d, s, u) {
+              w.RocketChat = function(c) { w.RocketChat._.push(c) }; w.RocketChat._ = []; w.RocketChat.url = u;
+              var h = d.getElementsByTagName(s)[0], j = d.createElement(s);
+              j.async = true; j.src = 'https://chat.nathfreder.dev/livechat/rocketchat-livechat.min.js?_=201903270000';
+              h.parentNode.insertBefore(j, h);
+            })(window, document, 'script', 'https://chat.nathfreder.dev/livechat');
             `,
           }}
         />
-        <script __dangero>  </script>
-        <script id="intergram" type="text/javascript" src="https://www.intergram.xyz/js/widget.js"></script>
         <noscript><img src="https://api.simpleanalytics.io/hello.gif" alt=""/></noscript> 
       </body>
     </html>
