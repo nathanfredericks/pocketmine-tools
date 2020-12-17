8d0663d5b4fc22821099ec7d6de0625f22c5639b
Nathaniel Fredericks <fna371858@gmail.com>
2020-12-17 18:18:26 -0400

    Fix meta in _document error

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,7 +1,28 @@
 import 'bootstrap/scss/bootstrap.scss';
+import Head from 'next/head';
+import React from 'react';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta
+          name="description"
+          content="Create and extract PocketMine plugins online"
+        />
+        <meta
+          name="keywords"
+          content="PocketMine,PocketMine Tools,PMT,pmt.mcpe.fun"
+        />
+        <meta name="author" content="Nathaniel Fredericks" />
+        <meta name="theme-color" content="#eeeeee" />
+        <meta property="og:image" content="/static/logo.png" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
