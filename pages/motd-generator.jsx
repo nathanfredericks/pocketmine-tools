71fdb95117a22217cda4e49dae50bb6166ef5f66
Nathan Fredericks <nathan@fredericks.dev>
2020-12-30 20:01:23 -0400

    Open MOTD Generator in new tab

--- pages/motd-generator.jsx
+++ pages/motd-generator.jsx
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
 import Layout from '../components/Layout';
+import { Button } from 'react-bootstrap';
 
 export default class extends Component {
   render = () => (
     <Layout title="MOTD Generator">
+      <a href="https://motd-generator.nathfreder.dev/">
+        <Button variant="secondary">
+          Open in new tab
+        </Button>
+      </a>
       <iframe width="100%" height="500px" frameBorder="0" src="https://motd-generator.nathfreder.dev/" title="MOTD Generator" />
     </Layout>
   );
