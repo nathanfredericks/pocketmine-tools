d2fbf4aaf5ca03196f54357ab44fb0f5ad8e26cf
Nathan Fredericks <nathan@nathfreder.dev>
2023-06-07 14:55:20 -0300

    Remove crashdump validation

--- pages/crashdump-parser.jsx
+++ pages/crashdump-parser.jsx
@@ -1,38 +1,25 @@
-import React, { Component, SyntheticEvent } from 'react';
+import React, { Component } from 'react';
 import { Alert, Button, Form, Tab, Tabs } from 'react-bootstrap';
 import { saveAs } from 'file-saver';
 import Layout from '../components/Layout';
-import Crashdump from '../lib/crashdump.interface';
 import Link from 'next/link';
 import dynamic from 'next/dynamic';
 import Head from 'next/head';
-type CrashdumpParserState = {
-  parseError: string | null;
-  parseErrorLink: string | null;
-  previewError: string | null;
-  previewErrorLink: string | null;
-  parsedCrashdumpStr: string | null;
-  parsedCrashdumpObj: Crashdump | null;
-  loading: boolean;
-  crashdump: string | null;
-};
 export default class CrashdumpParser extends Component {
-  state: CrashdumpParserState = {
+  state = {
     parseError: null,
     parseErrorLink: null,
-    previewError: null,
-    previewErrorLink: null,
     parsedCrashdumpStr: null,
     parsedCrashdumpObj: null,
     loading: false,
     crashdump: null,
   };
-  handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  handleChange = (event) => {
     this.setState({
       crashdump: event.currentTarget.value,
     });
   };
-  handleSubmit = async (event: SyntheticEvent) => {
+  handleSubmit = async (event) => {
     event.preventDefault();
     this.setState({
       loading: true,
@@ -49,7 +36,7 @@ export default class CrashdumpParser extends Component {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({
-        crashdump: this.state.crashdump!,
+        crashdump: this.state.crashdump,
       }),
     });
     const json = await response.json();
@@ -60,24 +47,14 @@ export default class CrashdumpParser extends Component {
         parseErrorLink: '/support#decode-crashdump-error'
       });
     }
-    if (json.preview) {
-      this.setState({
-        parsedCrashdumpObj: JSON.parse(json.crashdump),
-      });
-    } else {
-      this.setState({
-        previewError:
-          'Sorry, your crashdump could not be previewed. Raw JSON is still available.',
-        previewErrorLink: '/support#preview-not-avail-error'
-      });
-    }
     this.setState({
       loading: false,
       parsedCrashdumpStr: json.crashdump,
+      parsedCrashdumpObj: JSON.parse(json.crashdump),
     });
   };
   saveCrashdump = () => {
-    const blob = new Blob([this.state.parsedCrashdumpStr!], {
+    const blob = new Blob([this.state.parsedCrashdumpStr], {
       type: 'application/json;charset=utf-8',
     });
     saveAs(blob, 'crashdump.json');
@@ -86,15 +63,13 @@ export default class CrashdumpParser extends Component {
     const {
       parseError,
       parseErrorLink,
-      previewError,
-      previewErrorLink,
       parsedCrashdumpStr,
       parsedCrashdumpObj,
       loading,
       crashdump,
     } = this.state;
-    let CrashdumpPreview: any = null;
-    if (parsedCrashdumpStr && !previewError) {
+    let CrashdumpPreview = null;
+    if (parsedCrashdumpStr) {
       CrashdumpPreview = dynamic(import('../components/CrashdumpPreview'), {
         loading: () => <p>Loading preview<span className="dots" /></p>,
       });
@@ -106,7 +81,7 @@ export default class CrashdumpParser extends Component {
         </Head>
         <Layout title="Crashdump Parser" showNav={true}>
           {parseError ?
-            <Alert variant="danger">{parseError} <Link href={parseErrorLink!}>More info.</Link></Alert> : null}
+            <Alert variant="danger">{parseError} <Link href={parseErrorLink}>More info.</Link></Alert> : null}
           <Form onSubmit={this.handleSubmit}>
             <Form.Group>
               <Form.Label>Crashdump</Form.Label>
@@ -141,11 +116,7 @@ export default class CrashdumpParser extends Component {
           {parsedCrashdumpStr ? (
             <Tabs defaultActiveKey="preview" className="mb-3 mt-3">
               <Tab eventKey="preview" title="Preview">
-                {previewError ? (
-                  <Alert variant="danger">{previewError} <Link href={previewErrorLink!}>More info.</Link></Alert>
-                ) : (
-                  <CrashdumpPreview crashdump={parsedCrashdumpObj!} />
-                )}
+                <CrashdumpPreview crashdump={parsedCrashdumpObj} />
               </Tab>
               <Tab eventKey="raw" title="Raw JSON">
                 <Form.Group>
