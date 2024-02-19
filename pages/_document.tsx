2cc001495f6af952784f54b9befc1c64b14d97c6
Nathan Fredericks <nathan@fredericks.dev>
2024-02-18 21:03:34 -0400

    Add Cloudflare Web Analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,6 +6,7 @@ export default function Document() {
       <body>
       <Main />
       <NextScript />
+      <script defer src='https://static.cloudflareinsights.com/beacon.min.js' data-cf-beacon='{"token": "a19dd734f701418ca061efc56d180b99"}'></script>
       </body>
     </Html>
   );
