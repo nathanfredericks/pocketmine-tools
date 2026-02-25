040fe0152a82c9b9e45b74e85fb4a68402e9dfba
Nathan Fredericks <nathan@fredericks.dev>
2026-02-25 16:04:23 -0400

    Add AdSense script

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,7 +2,13 @@ import { Head, Html, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html>
-      <Head />
+      <Head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1958001180266248"
+          crossOrigin="anonymous"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
